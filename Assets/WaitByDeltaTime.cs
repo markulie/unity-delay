7267ea39761dc508f16a7bd9b0291e23b34a6d80
markhuna <markhuna@gmail.com>
2022-10-09 23:00:52 +0400

    u

---
+++ Assets/WaitByDeltaTime.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class WaitByDeltaTime : MonoBehaviour
+{
+    public float time = 0;
+    bool used = false;
+
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time > 10)
+        {
+            Debug.Log(2);
+            Destroy(gameObject);
+        }
+        else if (time > 5 && !used)
+        {
+            Debug.Log(1);
+            used = true;
+        }
+    }
+}
---
+++ Assets/WaitByIEnumerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WaitByIEnumerator : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(Wait(5, "1"));
+        StartCoroutine(Wait(10, "2"));
+    }
+
+    IEnumerator Wait(float sec, string msg)
+    {
+        yield return new WaitForSeconds(sec);
+        Debug.Log(msg);
+    }
+}
